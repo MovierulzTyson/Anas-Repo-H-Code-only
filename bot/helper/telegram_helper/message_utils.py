86f1bf12494ff58b37c01e6f67842c218c75603b
Ahad Shaikh <93983627+ahadz-git@users.noreply.github.com>
2022-06-25 11:32:24 +0530

    Update message_utils.py

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -36,6 +36,31 @@ def sendMarkup(text: str, bot, message: Message, reply_markup: InlineKeyboardMar
         LOGGER.error(str(e))
         return
 
+def sendPrivateMsg(text: str, bot, message: Message):
+    try:
+        return bot.sendMessage(message.from_user.id,
+                            text=text, parse_mode='HTMl', disable_web_page_preview=True)
+    except RetryAfter as r:
+        LOGGER.warning(str(r))
+        sleep(r.retry_after * 1.5)
+        return sendPrivateMsg(text, bot, message)
+    except Exception as e:
+        LOGGER.error(str(e))
+        return
+
+def sendPrivateMarkup(text: str, bot, message: Message, reply_markup: InlineKeyboardMarkup):
+    try:
+        return bot.sendMessage(message.from_user.id,
+                            text=text, reply_markup=reply_markup,
+                            parse_mode='HTMl', disable_web_page_preview=True)
+    except RetryAfter as r:
+        LOGGER.warning(str(r))
+        sleep(r.retry_after * 1.5)
+        return sendPrivateMarkup(text, bot, message, reply_markup)
+    except Exception as e:
+        LOGGER.error(str(e))
+        return
+
 def editMessage(text: str, message: Message, reply_markup=None):
     try:
         bot.editMessageText(text=text, message_id=message.message_id,
